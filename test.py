72bde3783e0aa902443ef70cc967594999027aa2
hankug1234 <hankug12@gmail.com>
2025-07-21 22:37:07 +0900

    파싱 테스트중

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-from auto_prove import Formula, Constant, Var, Function, Predicate, Operation, operation, is_operation, is_atom
+from auto_prove import Formula, Constant, Var, Function, Predicate, Operation, operation, is_operation, is_atom, is_term, is_formula
 from collections import deque 
-from typing import Tuple,List
+from typing import Tuple,List,Union
 import re,sys
 
 sys.setrecursionlimit(10000)
@@ -11,51 +11,64 @@ class String2FormulaConvertException(Exception):
     def __init__(self,message):
         super.__init__(message)
 
+def _primitive(value:str)->Union[bool,Var,Constant]:
+    if _variation_pattern.fullmatch(value):
+        return Var(value)
+    elif len(value) >= 1:
+        if value == "false":
+            return False
+        elif value == "true":
+            return True
+        else:
+            return Constant(value)
+    else:
+        return None
+
 def _formula(formula:str) -> Tuple[Formula, str]:
-    value = ""
-    remain = None
     params = []
+    value, remain = "", ""
     i = 0
     while i < len(formula):
         ch = formula[i]
         if ch == "(":
-            formula, remain = _formula(formula[i+1:])
-            params.append(formula)
+            _params, remain = _formula(formula[i+1:])
+            if re.fullmatch(_variation_pattern,value):
+                sub_formula = Predicate(value,list(_params))
+            elif len(value) >= 1:
+                sub_formula = Function(value,list(_params))
+            else:
+                sub_formula = _params
+            params.append(sub_formula)
             formula = remain
             i = 0
+            value=""
             continue
         elif ch == ")":
-            if value:
-                if value[0].isupper():
-                    return (Predicate(value,params), formula[i+1:])
-                else:
-                    return (Function(value,params), formula[i+1:])
-            else:
-                return (tuple(params), formula[i+1:])
-        elif ch == ',' or is_operation(ch) or i+1 == len(formula):
+            
+            primitive = _primitive(value)
+            if primitive is not None:
+                params.append(primitive)
+            
+            return (tuple(params), formula[i+1:])
+        elif ch == ',' or ch.isspace() or is_operation(ch) or i+1 == len(formula):
                                 
-            if _variation_pattern.fullmatch(value):
-                params.append(Var(value))
-            elif len(value) >= 1:
-                if value == "false":
-                    params.append(False)
-                elif value == "true":
-                    params.append(True)
-                else:
-                    params.append(Constant(value))
-                value = ""
+            primitive = _primitive(value)
+            if primitive is not None:
+                params.append(primitive)
+                
             if is_operation(ch):
                 params.append(operation(ch))
+            value = ""
+            
         else:
-            if ch != ' ':
-                value+=ch
+            value+=ch
         i+=1           
 
     return (tuple(params),remain)
 
 def _pre_modification(formula: Formula)->Formula:
 
-    if is_atom(formula):
+    if is_atom(formula) or is_term(formula):
         return formula
     else:
         temp = []
@@ -64,12 +77,29 @@ def _pre_modification(formula: Formula)->Formula:
             f = queue.popleft()
             if isinstance(f,Operation):
                 if f.is_binary_ops():
-                    temp.append((f,temp.pop(),_pre_modification(queue.appendleft())))
+                    return (f,temp.pop(),_pre_modification(tuple(queue)))
+                elif f.is_quantifiers():
+                    return (f,queue.popleft(), _pre_modification(tuple(queue)))
                 else:
-                    temp.append((f,_pre_modification(queue.appendleft())))
+                    return (f,_pre_modification(tuple(queue)))
             else:
                 temp.append(_pre_modification(f))
         return temp[0]
+    
+def _seperate_premises(premises:List[Formula]) -> List[Tuple]:
+    stack = []
+    seperated = []
+    for formula in premises:
+        if len(stack) != 0 and is_formula(stack[-1]) and not is_formula(formula) :
+            seperated.append(tuple(stack))
+            stack = []    
+            stack.append(formula)
+        else:
+            stack.append(formula)
+                    
+    if len(stack) > 0:
+        seperated.append(tuple(stack))
+    return seperated 
 
 def str2formula(fol:str) -> Tuple[List[Formula], Formula]:
     
@@ -77,10 +107,12 @@ def str2formula(fol:str) -> Tuple[List[Formula], Formula]:
     
     if conclusion in fol:
         premises, goal = fol.split(conclusion)
-        premises = _formula(premises)
-        goal = _formula(goal)
+        premises,_ = _formula(premises)
+        premises = _seperate_premises(premises)
+        goal,_ = _formula(goal)
         return ([_pre_modification(premise) for premise in premises ], _pre_modification(goal)) 
-    goal = _formula(fol)
+    goal,_ = _formula(fol)
+    print(goal)
     return ([], _pre_modification(goal))
 
 
