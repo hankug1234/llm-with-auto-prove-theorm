3ca191cad6ce37e50eb649b38a5de0e38e4f42d8
hankug1234 <hankug12@gmail.com>
2025-07-27 23:18:45 +0900

    interrupt 처리 코드 추가 해야함

---
+++ main.py
@@ -0,0 +1,24 @@
+from agent import ATPagent 
+
+
+if __name__ == "__main__":
+    user_instruction = {
+                    "{{CONCEPT}}" : "",
+                    "{{USER_INSTRUCTION}}":"",
+                    "{{INPUT_FORMAT}}":"",
+                    "{{OUTPUT_FORMAT}}":"",
+                    "{{RULES}}":"",
+                    "{{EXAMPLES}}":""
+                 }
+    agent = ATPagent(user_instruction=user_instruction)
+    session = agent.get_sesesion()
+    
+    while True:
+        user_input = input("사용자 입력")
+        response = session(user_input)
+        if response is None:
+            break 
+        else:
+            print(response)
+        
+        
